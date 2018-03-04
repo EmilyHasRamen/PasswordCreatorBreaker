b55b870a29cfc9836484dcca4ae28de6db965da5
Joanna <admin@admins-MacBook-Air.local>
2018-03-03 20:01:20 -0800

    scraper

---
+++ scraper/Server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+
+app.get('/scrape', function(req, res) {
+
+// url below
+
+url = ''
+
+})
+
+exports = module.exports = app;
